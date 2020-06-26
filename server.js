cf295e20699a82212583a690be0368ffc66d47b9
Qudusayo <zeekimono@gmail.com>
2020-06-26 11:50:57 +0100

    Added auto server start and console messages

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const child_process = require('child_process');
 const server = express();
 const port = 8900 || process.env.PORT;
+const url = `http://localhost:${port}`;
 
 server.use(express.static(path.join(__dirname, 'static')));
 server.get('/', (req, res) => {
@@ -28,11 +30,13 @@ server.get('/files', (req, res) => {
     });
 })
 
-server.listen(port, () => console.log(
-    `Server started on port ${port}
-
-CSS link is http://localhost:${port}/css/your_css_file_name e.g http://localhost:${port}/css/bootstrap.min.css
-
-Javascript link is http://localhost:${port}/js/your_js_file_name e.g http://localhost:${port}/js/bootstrap.min.js
-`
-))
+server.listen(port, () => {
+    console.log('\x1b[36m%s\x1b[0m', 'Starting the development server\n')
+    console.log('You can now get your packages in the browser\n')
+    console.log('\tLocal:\t\thttp://localhost:8900/\n')
+    console.log("\x1b[31m", 'Press [Ctrl + C] to abort the server')
+    var start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
+    setTimeout(() => {
+        child_process.exec(start + ' ' + url);
+    }, 3000)
+})
